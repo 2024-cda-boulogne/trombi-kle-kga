66f280110f820f0c1ab831a282bae82b5654c84d
Karl-Gavois <Gavois.k@hotmail.com>
2024-03-20 13:55:34 +0100

    feat: :art: Add TOUT
    TOUT

--- JS/test.js
+++ JS/test.js
@@ -37,10 +37,29 @@ function zoomOnClick(svgElement, zoomFactor, otherSvgElement) {
 var svgElement1 = document.querySelector('.FR-62');
 var svgElement2 = document.querySelector('.FR-59');
 zoomOnClick(svgElement1, 13, svgElement2); // Définir le scale à 2 lors du clic
-zoomOnClick(svgElement2, 11, svgElement1); // Définir le scale à 2 lors du clic
+zoomOnClick(svgElement2, 13, svgElement1); // Définir le scale à 2 lors du clic
 
-
-function showPopup() {
+document.addEventListener("DOMContentLoaded", function() {
+    var points = document.querySelectorAll(".point");
+    points.forEach(function(point) {
+      point.addEventListener("click", function() {
+        var city = this.getAttribute("data-user-id");
+        showPopup(city);
+      });
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    var points = document.querySelectorAll(".point");
+    points.forEach(function(point) {
+      point.addEventListener("click", function() {
+        var city = this.getAttribute("data-city");
+        showPopup(city);
+      });
+    });
+  });
+  
+  function showPopup(city) {
     var popup = document.getElementById("popup");
     popup.style.display = "block";
     
@@ -48,13 +67,26 @@ function showPopup() {
     fetch('data.json')
       .then(response => response.json())
       .then(data => {
-        var popupContent = document.getElementById("popupContent");
-        popupContent.innerHTML = "<h2>" + data.title + "</h2><p>" + data.description + "</p>";
+        // Filter users by the given city
+        var usersInCity = data.users.filter(item => item.city === city);
+        if (usersInCity.length > 0) {
+          var popupContent = document.getElementById("popupContent");
+          // Generate HTML for each user in the city
+          var contentHTML = "<h2>Users in " + city + "</h2>";
+          usersInCity.forEach(function(user) {
+            contentHTML += "<p>Name: " + user.name + "</p><p>Stack: " + user.stack + "</p><br>";
+          });
+          popupContent.innerHTML = contentHTML;
+        } else {
+          console.error("No users found for city:", city);
+        }
       })
       .catch(error => console.error('Error fetching JSON:', error));
   }
+  
   
   function closePopup() {
     var popup = document.getElementById("popup");
     popup.style.display = "none";
   }
+  
