89a39ab05bc5d957ab6a12d5b7e75650a98de0d5
Karl-Gavois <Gavois.k@hotmail.com>
2024-03-20 10:57:06 +0100

    feat: :art: Add Maps
    This is a description of the maps

--- JS/test.js
+++ JS/test.js
@@ -1,5 +1,43 @@
-let map62 = document.querySelector("#IMG62");
+function zoomOnClick(svgElement, zoomFactor, elementToHide) {
+    let originalTransform = svgElement.getAttribute('transform');
+    let isZoomed = false;
 
-map62.addEventListener("click", function() {
-    alert("Clique sur l'image");
-})
+    svgElement.addEventListener('click', function(event) {
+        if (isZoomed) {
+            // Rétablir la taille initiale avec une transition de 0.5 secondes
+            svgElement.style.transition = 'transform 0.5s ease';
+            svgElement.style.transform = originalTransform;
+            elementToHide.style.transition = 'opacity 0.5s ease';
+            elementToHide.style.opacity = '1';
+            isZoomed = false;
+        } else {
+            // Appliquer le zoom sur l'élément SVG avec une transition de 0.5 secondes
+            let zoomTransform = 'translate(' + -2950 + ',' + 0 + ') scale(' + zoomFactor + ')';
+            svgElement.style.transition = 'transform 0.5s ease';
+            svgElement.style.transform = zoomTransform;
+            elementToHide.style.transition = 'opacity 0.5s ease';
+            elementToHide.style.opacity = '0';
+            isZoomed = true;
+        }
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!svgElement.contains(event.target)) {
+            // Rétablir la taille initiale avec une transition de 0.5 secondes
+            svgElement.style.transition = 'transform 0.5s ease';
+            svgElement.style.transform = originalTransform;
+            elementToHide.style.transition = 'opacity 0.5s ease';
+            elementToHide.style.opacity = '1';
+            isZoomed = false;
+        }
+    });
+}
+
+// Exemple d'utilisation :
+var svgElement1 = document.querySelector('.FR-62');
+var elementToHide1 = document.querySelector('.FR-59');
+zoomOnClick(svgElement1, 13, elementToHide1);
+
+var svgElement2 = document.querySelector('.FR-59');
+var elementToHide2 = document.querySelector('.FR-62');
+zoomOnClick(svgElement2, 11, elementToHide2);
